3c4607af6cef788eec29a48b78a56ec6890368f1
Priyanshu Sundriyal <priyanshusundriyal65@gmail.com>
2025-10-31 00:48:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+# use model
+model = YOLO('model.pt')
+
+
+# using camera
+while True:
+    model.predict(source=0, imgsz=640, conf=0.6, show=True )
+
+
+
+
+
+# using camera
+model.predict(source=0, imgsz=640, conf=0.6, show=True )
+
+
+
+# using image
+# model.predict(source='assests/example_2.jpg', imgsz=640, conf=0.6, save=True)
